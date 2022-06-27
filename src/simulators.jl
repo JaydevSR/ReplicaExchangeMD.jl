4c8668b2e07a33130ab2e5c313b374b0f7063a3f
JaydevSR <jaydevsr7701@gmail.com>
2022-06-27 11:50:36 +0530

    add `exchange_time` to `TemperatureREMD`

--- src/simulators.jl
+++ src/simulators.jl
@@ -1,84 +1,55 @@
 export TemperatureREMD
-struct TemperatureREMD{N, T, S, SD}
+struct TemperatureREMD{N, T, S, SD, ET, DT}
     temps::StaticVector{N, T}
-    indices::StaticVector{N, Int}
     simulators::SD
+    exchange_time::ET
+    dt::DT
 end
 
 n_simulators(::TemperatureREMD{N}) where {N} = N 
 
 function TemperatureREMD(;
-    simulator,
-    temps, dt, 
+    simulators, exchange_time, dt, temps,
     kwargs...)
-    S = simulator
+    S = eltype(simulators)
     T = eltype(temps)
     N = length(temps)
+    ET = typeof(exchange_time)
+    DT = typeof(dt)
     temps = SA[temps...]
-    indices = SA[collect(1:length(temps))...]
-
-    coupling_required = false
-    if simulator ∈ [VelocityVerlet, Verlet, StormerVerlet]
-        coupling_required = true
-        coupling = kwargs[:coupling]
-    end
-
-    if coupling_required && coupling ∈ [AndersenThermostat, BerendsenThermostat]
-        couplings = [coupling(temps[i], kwargs[:coupling_const]) for i in indices]
-    elseif coupling_required && coupling ∈ [RescaleThermostat]
-        couplings = [coupling(temps[i]) for i in indices]
-    elseif coupling_required
-        error("Temperature coupling required for TemperatureREMD using $(simulator) simulator.")
-    end
-
-    if simulator==Langevin
-        simulators = Dict(
-            [i, simulator(dt=dt, temperature=temps[i], friction=kwargs[:friction],
-            remove_CM_motion=kwargs[:remove_CM_motion])]
-            for i in indices
-        )
-    elseif simulator ∈ [VelocityVerlet, Verlet]
-        simulators = Dict(
-            [i, simulator(dt=dt, temperature=temps[i], coupling=couplings[i],
-            remove_CM_motion=kwargs[:remove_CM_motion])]
-            for i in indices
-        )
-    elseif simulator == StormerVerlet
-        simulators = Dict(
-            [i, simulator(dt=dt, temperature=temps[i], coupling=couplings[i])]
-            for i in indices
-        )
-    else
-        error("Simulator type $(simulator) not supported.")
-    end
+    
+    simulators = Dict([i, simulators[i]] for i in 1:N)
     SD = typeof(simulators)
 
-    return TemperatureREMD{N, T, S, SD}(temps, indices, simulators)
+    return TemperatureREMD{N, T, S, SD, ET}(temps, simulators, exchange_time, dt)
 end
 
 # simulate temperature replica exchange
 function simulate!(;
     sys::ReplicaSystem,
     sim::TemperatureREMD,
-    n_cycles::Int,
-    cycle_length::Int;
+    n_steps,
     parallel=true
     )
 
-    if sys.n_replicas != n_simulators(sim)
+    if sys.n_replicas != length(sim.simulators)
         error("Number of replicas in ReplicaSystem and simulators in TemperatureREMD must match.")
     end
+
+    # calculate n_cycles and n_steps_per_cycle from dt and exchange_time
+    n_cycles = (n_steps * sim.dt) ÷ sim.exchange_time
+    cycle_length = n_steps ÷ n_cycles
+    remaining_steps = n_steps % n_cycles
+
     for cycle=1:n_cycles
-        for idx in sim.indices  # FLAG: this needs to be parallelized
-            # sys.replicas[i] is IndividualReplica not System
-            simulate!(sys.replicas[idx], sim.simulators[idx], cycle_length; parallel=parallel)  # FLAG: not working
+        for idx in eachindex(sim.simulators)  # FLAG: this needs to be parallelized
+            simulate!(sys.replicas[idx], sim.simulators[idx], cycle_length; parallel=parallel)  # FLAG: check if this is working
         end
 
         if cycle != n_cycles
             n = rand(1:sys.n_replicas)
             m = mod(n, sys.n_replicas) + 1
             β_n, β_m = 1/sim.temps[n], 1/sim.temps[m]
-            # sys.replicas[i] is IndividualReplica not System
             V_n, V_m = potential_energy(sys.replicas[n]), potential_energy(sys.replicas[m])  # FLAG: not working
             Δ = (β_m - β_n)*(V_n - V_m)
             if Δ <= 0 || rand() < exp(-Δ)
@@ -88,5 +59,8 @@ function simulate!(;
                 sys.replicas[m].velocities *= sqrt(β_m/β_n)
             end
         end
+    end
+    for idx in eachindex(sim.simulators)  # FLAG: this needs to be parallelized
+        simulate!(sys.replicas[idx], sim.simulators[idx], remaining_steps; parallel=parallel)  # FLAG: check if this is working
     end
 end
