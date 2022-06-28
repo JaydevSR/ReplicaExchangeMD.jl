b09268cbc95bf3c83c39d54f5ea9b60a99dbea47
JaydevSR <jaydevsr7701@gmail.com>
2022-06-28 22:01:17 +0530

    add parallelization

--- src/simulators.jl
+++ src/simulators.jl
@@ -9,8 +9,10 @@ end
 n_simulators(::TemperatureREMD{N}) where {N} = N 
 
 function TemperatureREMD(;
-    simulators, exchange_time, dt, temps,
-    kwargs...)
+                temps,
+                simulators,
+                exchange_time,
+                dt, kwargs...)
     S = eltype(simulators)
     T = eltype(temps)
     N = length(temps)
@@ -23,37 +25,42 @@ function TemperatureREMD(;
     simulators = Dict([i, simulators[i]] for i in 1:N)
     SD = typeof(simulators)
     
-    return TemperatureREMD{N, T, S, SD, ET}(temps, simulators, exchange_time, dt)
+    return TemperatureREMD{N, T, S, SD, ET, DT}(temps, simulators, exchange_time, dt)
 end
 
 # simulate temperature replica exchange
-function simulate!(;
-    sys::ReplicaSystem,
-    sim::TemperatureREMD,
-    n_steps,
-    parallel=true
-    )
-
+function simulate!(sys::ReplicaSystem,
+                    sim::TemperatureREMD,
+                    n_steps::Integer;
+                    parallel::Bool=true)
     if sys.n_replicas != length(sim.simulators)
         error("Number of replicas in ReplicaSystem and simulators in TemperatureREMD must match.")
     end
     # calculate n_cycles and n_steps_per_cycle from dt and exchange_time
-    n_cycles = (n_steps * sim.dt) ÷ sim.exchange_time
+    n_cycles = convert(Int64, (n_steps * sim.dt) ÷ sim.exchange_time)
     cycle_length = n_steps ÷ n_cycles
     remaining_steps = n_steps % n_cycles
+    n_exchanges = 0
+    @info "Total number of cycles: $n_cycles (cycle length: $cycle_length, remaining steps: $remaining_steps)"
 
     for cycle=1:n_cycles
-        for idx in eachindex(sim.simulators)  # FLAG: this needs to be parallelized
-            simulate!(sys.replicas[idx], sim.simulators[idx], cycle_length; parallel=parallel)  # FLAG: check if this is working
+        @sync for idx in eachindex(sim.simulators)
+            if parallel
+                Threads.@spawn Molly.simulate!(sys.replicas[idx], sim.simulators[idx], cycle_length; parallel=parallel)
+            else
+                Molly.simulate!(sys.replicas[idx], sim.simulators[idx], cycle_length; parallel=parallel)
+            end
         end
 
+        #! Check replica exchage algorithm
         if cycle != n_cycles
             n = rand(1:sys.n_replicas)
             m = mod(n, sys.n_replicas) + 1
             β_n, β_m = 1/sim.temps[n], 1/sim.temps[m]
             V_n, V_m = potential_energy(sys.replicas[n]), potential_energy(sys.replicas[m])  # FLAG: not working
-            Δ = (β_m - β_n)*(V_n - V_m)
+            Δ = ustrip((β_m - β_n)*(V_n - V_m))
             if Δ <= 0 || rand() < exp(-Δ)
+                n_exchanges += 1
                 sys.replicas[n].coords, sys.replicas[m].coords = sys.replicas[m].coords, sys.replicas[n].coords
                 # scale velocities
                 sys.replicas[n].velocities *= sqrt(β_n/β_m)
@@ -61,7 +68,15 @@ function simulate!(;
             end
         end
     end
-    for idx in eachindex(sim.simulators)  # FLAG: this needs to be parallelized
-        simulate!(sys.replicas[idx], sim.simulators[idx], remaining_steps; parallel=parallel)  # FLAG: check if this is working
+    # run for remaining_steps (if >0) for all replicas
+    if remaining_steps > 0
+        @sync for idx in eachindex(sim.simulators)
+            if parallel
+                Threads.@spawn Molly.simulate!(sys.replicas[idx], sim.simulators[idx], remaining_steps; parallel=parallel)
+            else
+                Molly.simulate!(sys.replicas[idx], sim.simulators[idx], remaining_steps; parallel=parallel)
+            end
+        end
     end
+    @info "Number of exchanges: $n_exchanges"
 end
