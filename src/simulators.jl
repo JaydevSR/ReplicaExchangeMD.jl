ac0e93abf94d1ef4389b40de861487ea400cf0a9
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2022-08-10 10:44:40 +0530

    add general REMD

--- src/simulators.jl
+++ src/simulators.jl
@@ -1,4 +1,94 @@
-export TemperatureREMD
+export
+    REMD,
+    TemperatureREMD
+
+struct REMD{N,T,S,DT,RC,ST,ET}
+    dt::DT
+    lambdas::RC
+    simulators::ST
+    exchange_time::ET
+end
+
+function REMD(;
+    dt,
+    lambdas,
+    simulators,
+    exchange_time,
+    kwargs...)
+    S = eltype(simulators)
+    T = eltype(lambdas)
+    N = length(lambdas)
+    DT = typeof(dt)
+    RC = typeof(lambdas)
+    ET = typeof(exchange_time)
+    if length(simulators) != length(lambdas)
+        throw(ArgumentError("Number of values of the reaction coordinate must match the number of simulators"))
+    end
+    if exchange_time <= dt
+        throw(ArgumentError("Exchange time must be greater than the time step"))
+    end
+    simulators = Tuple(simulators[i] for i in 1:N)
+    ST = typeof(simulators)
+
+    return TemperatureREMD{N,T,S,DT,RC,ST,ET}(dt, lambdas, simulators, exchange_time)
+end
+
+reaction_coordinates(sim::REMD) = sim.lambdas
+
+function simulate!(sys::ReplicaSystem{D,G,T},
+    sim::REMD,
+    n_steps::Int,
+    make_exchange!::Function;
+    rng=Random.GLOBAL_RNG,
+    n_threads::Int=Threads.nthreads()) where {D,G,T}
+    if sys.n_replicas != length(sim.simulators)
+        throw(ArgumentError("Number of replicas in ReplicaSystem and simulators in the REMD simulator do not match."))
+    end
+
+    if n_threads > sys.n_replicas
+        thread_div = equal_parts(n_threads, sys.n_replicas)
+    else # pass 1 thread per replica
+        thread_div = equal_parts(sys.n_replicas, sys.n_replicas)
+    end
+
+    # calculate n_cycles and n_steps_per_cycle from dt and exchange_time
+    n_cycles = convert(Int, (n_steps * sim.dt) ÷ sim.exchange_time)
+    cycle_length = (n_cycles > 0) ? n_steps ÷ n_cycles : 0
+    remaining_steps = (n_cycles > 0) ? n_steps % n_cycles : n_steps
+    n_attempts = 0
+
+    for cycle = 1:n_cycles
+        ThreadsX.foreach(eachindex(sys.replicas, sim.simulators, thread_div)) do I
+            Molly.simulate!(sys.replicas[I], sim.simulators[I], cycle_length; n_threads=thread_div[I])
+        end
+
+        cycle_parity = cycle % 2
+        for n in 1+cycle_parity:2:sys.n_replicas-1
+            n_attempts += 1
+            m = n + 1
+            Δ, exchanged = make_exchange!(sys, reaction_coordinates(sim), n, m)
+            if exchanged && !isnothing(sys.exchange_logger)
+                Molly.log_property!(sys.exchange_logger, sys, nothing, cycle * cycle_length; indices=(n, m), delta=Δ, n_threads=n_threads)
+            end
+        end
+    end
+
+    # run for remaining_steps (if >0) for all replicas
+    if remaining_steps > 0
+        ThreadsX.foreach(eachindex(sys.replicas, sim.simulators, thread_div)) do I
+            Molly.simulate!(sys.replicas[I], sim.simulators[I], cycle_length; n_threads=thread_div[I])
+        end
+    end
+
+    if !isnothing(sys.exchange_logger)
+        finish_logs!(sys.exchange_logger; n_steps=n_steps, n_attempts=n_attempts)
+    end
+
+    return sys
+end
+
+
+
 """
     TemperatureREMD(; <keyword arguments>)
 
@@ -11,7 +101,7 @@ arguments:
 - `simulators::ST`: individual simulators for simulating each replica.
 - `exchange_time::ET`: the time interval between replica exchange attempt.
 """
-struct TemperatureREMD{N,T,S,DT,TP,ST,ET}
+struct TemperatureREMD{N,T,S,DT,TP,ST,ET} <: REMD{N,T,S,DT,TP,ST,ET}
     dt::DT
     temperatures::TP
     simulators::ST
@@ -42,6 +132,8 @@ function TemperatureREMD(;
     return TemperatureREMD{N,T,S,DT,TP,ST,ET}(dt, temperatures, simulators, exchange_time)
 end
 
+reaction_coordinates(sim::TemperatureREMD) = sim.temperatures
+
 function simulate!(sys::ReplicaSystem{D,G,T},
     sim::TemperatureREMD,
     n_steps::Int;
@@ -49,71 +141,38 @@ function simulate!(sys::ReplicaSystem{D,G,T},
     rng=Random.GLOBAL_RNG,
     n_threads::Int=Threads.nthreads()) where {D,G,T}
     if sys.n_replicas != length(sim.simulators)
-        throw(ArgumentError("Number of replicas in ReplicaSystem and simulators in TemperatureREMD do not match."))
+        throw(ArgumentError("Number of replicas in ReplicaSystem and simulators in the REMD simulator do not match."))
     end
-
-    if n_threads > sys.n_replicas
-        thread_div = equal_parts(n_threads, sys.n_replicas)
-    else # pass 1 thread per replica
-        thread_div = equal_parts(sys.n_replicas, sys.n_replicas)
-    end
-
-    # calculate n_cycles and n_steps_per_cycle from dt and exchange_time
-    n_cycles = convert(Int, (n_steps * sim.dt) ÷ sim.exchange_time)
-    cycle_length = (n_cycles > 0) ? n_steps ÷ n_cycles : 0
-    remaining_steps = (n_cycles > 0) ? n_steps % n_cycles : n_steps
-    n_attempts = 0
 
     if assign_velocities
         for i in eachindex(sys.replicas)
-            random_velocities!(sys.replicas[i], sim.temperatures[i]; rng=rng)
+            random_velocities!(sys.replicas[i], reaction_coordinates(sim)[i]; rng=rng)
         end
     end
 
-    for cycle = 1:n_cycles
-        ThreadsX.foreach(eachindex(sys.replicas, sim.simulators, thread_div)) do I
-            Molly.simulate!(sys.replicas[I], sim.simulators[I], cycle_length; n_threads=thread_div[I])
-        end
+    simulate!(sys, sim, n_steps, tremd_exchange!; rng=rng, n_threads=n_threads)
+end
 
-        cycle_parity = cycle % 2
-        for n in 1+cycle_parity:2:sys.n_replicas-1
-            n_attempts += 1
-            m = n + 1
-            if dimension(sys.energy_units) == u"𝐋^2 * 𝐌 * 𝐍^-1 * 𝐓^-2"
-                k_b = sys.k * T(Unitful.Na)
-            else
-                k_b = sys.k
-            end
-            T_n, T_m = sim.temperatures[n], sim.temperatures[m]
-            β_n, β_m = 1 / (k_b * T_n), 1 / (k_b * T_m)
-            neighbors_n = find_neighbors(sys.replicas[n], sys.replicas[n].neighbor_finder; n_threads=n_threads)
-            neighbors_m = find_neighbors(sys.replicas[m], sys.replicas[m].neighbor_finder; n_threads=n_threads)
-            V_n, V_m = potential_energy(sys.replicas[n], neighbors_n), potential_energy(sys.replicas[m], neighbors_m)
-            Δ = (β_m - β_n) * (V_n - V_m)
-            if Δ <= 0 || rand(rng) < exp(-Δ)
-                # exchange coordinates and velocities
-                sys.replicas[n].coords, sys.replicas[m].coords = sys.replicas[m].coords, sys.replicas[n].coords
-                sys.replicas[n].velocities, sys.replicas[m].velocities = sys.replicas[m].velocities, sys.replicas[n].velocities
-                # scale velocities
-                sys.replicas[n].velocities .*= sqrt(T_n / T_m)
-                sys.replicas[m].velocities .*= sqrt(T_m / T_n)
-                if !isnothing(sys.exchange_logger)
-                    Molly.log_property!(sys.exchange_logger, sys, nothing, cycle * cycle_length; indices=(n, m), delta=Δ, n_threads=n_threads)
-                end
-            end
-        end
+function tremd_exchange!(sys::ReplicaSystem{T}, temperatures::Vector{T}, n::Integer, m::Integer) where {T}
+    if dimension(sys.energy_units) == u"𝐋^2 * 𝐌 * 𝐍^-1 * 𝐓^-2"
+        k_b = sys.k * T(Unitful.Na)
+    else
+        k_b = sys.k
     end
-
-    # run for remaining_steps (if >0) for all replicas
-    if remaining_steps > 0
-        ThreadsX.foreach(eachindex(sys.replicas, sim.simulators, thread_div)) do I
-            Molly.simulate!(sys.replicas[I], sim.simulators[I], cycle_length; n_threads=thread_div[I])
-        end
+    T_n, T_m = temperatures[n], temperatures[m]
+    β_n, β_m = 1 / (k_b * T_n), 1 / (k_b * T_m)
+    neighbors_n = find_neighbors(sys.replicas[n], sys.replicas[n].neighbor_finder; n_threads=n_threads)
+    neighbors_m = find_neighbors(sys.replicas[m], sys.replicas[m].neighbor_finder; n_threads=n_threads)
+    V_n, V_m = potential_energy(sys.replicas[n], neighbors_n), potential_energy(sys.replicas[m], neighbors_m)
+    Δ = (β_m - β_n) * (V_n - V_m)
+    should_exchange = Δ <= 0 || rand(rng) < exp(-Δ)
+    if should_exchange
+        # exchange coordinates and velocities
+        sys.replicas[n].coords, sys.replicas[m].coords = sys.replicas[m].coords, sys.replicas[n].coords
+        sys.replicas[n].velocities, sys.replicas[m].velocities = sys.replicas[m].velocities, sys.replicas[n].velocities
+        # scale velocities
+        sys.replicas[n].velocities .*= sqrt(T_n / T_m)
+        sys.replicas[m].velocities .*= sqrt(T_m / T_n)
     end
-
-    if !isnothing(sys.exchange_logger)
-        finish_logs!(sys.exchange_logger; n_steps=n_steps, n_attempts=n_attempts)
-    end
-
-    return sys
+    return Δ, should_exchange
 end
